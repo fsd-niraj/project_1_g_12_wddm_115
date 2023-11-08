4bb824b7ab896108038c912583121e0a5bcf4fb9
fsd-niraj <develop.niraj@gmail.com>
2023-11-08 16:36:14 -0500

    added bottom section of the card with dynamic data

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ const restaurantData = [
 function displayRestaurantCards() {
   const restaurantList = document.getElementById("restaurant_list");
 
-  restaurantData.map((data, i)=> {
+  restaurantData.map((data, i) => {
     const detailCard = document.createElement("div")
     detailCard.classList.add('card', 'animate-up')
     detailCard.id = i
@@ -89,15 +89,39 @@ function displayRestaurantCards() {
     const starDiv = document.createElement("div")
     starDiv.classList.add('star')
     starDiv.textContent = data.star
-    
+
     const nameStarDiv = document.createElement("div")
+    nameStarDiv.classList.add('d-flex', 'align-center', 'justify-between')
+    nameStarDiv.appendChild(nameDiv)
+    nameStarDiv.appendChild(starDiv)
+
     const textDiv = document.createElement("div")
+    textDiv.classList.add('text')
+    textDiv.textContent = data.cuisine
     const rtextDiv = document.createElement("div")
+    rtextDiv.classList.add('rtext')
+    rtextDiv.textContent = data.cost
+
     const infoTextDiv = document.createElement("div")
+    infoTextDiv.classList.add('d-flex', 'align-center', 'justify-between')
+    infoTextDiv.appendChild(textDiv)
+    infoTextDiv.appendChild(rtextDiv)
+
     const infoDiv = document.createElement("div")
+    infoDiv.classList.add('info')
+    infoDiv.textContent = data.info
     const safetyDiv = document.createElement("div")
+    safetyDiv.classList.add('safety')
+
     const safetyTextDiv = document.createElement("div")
+    safetyTextDiv.classList.add('d-flex', 'align-center', 'justify-between')
+    safetyTextDiv.appendChild(infoDiv)
+    safetyTextDiv.appendChild(safetyDiv)
+
     const safetyImg = document.createElement("img")
+    safetyImg.src = "images/safety.png"
+    safetyDiv.appendChild(safetyImg)
+    
     const combinedData = document.createDocumentFragment();
     detailCard.appendChild(combinedData);
     restaurantList.appendChild(detailCard)
